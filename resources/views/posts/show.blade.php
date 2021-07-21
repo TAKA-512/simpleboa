411b6efb4eb56f5dbb74cd45ee11e4f9edec6289
TAKA-512 <tbctkhr0512@gmail.com>
2021-07-21 22:46:58 +0900

    first commit

---
+++ resources/views/posts/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.layouts')
+
+@section('title', 'Simple Board')
+
+@section('content')
+@if (session('message')) 
+    {{ session('message') }} 
+@endif 
+    {{ $post->title }} 
+    {{ $post->content }}
+    <a href="/simpleboard/posts/{{ $post->id }}/edit">Edit</a>
+@endsection
